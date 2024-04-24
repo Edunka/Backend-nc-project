8e2bbec0708bae2f2a57a49784ea1ea74367765a
Edunka <emil.dunka96@gmail.com>
2024-04-24 15:06:04 +0100

    still working on hosting

--- db/connection.js
+++ db/connection.js
@@ -5,9 +5,6 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
-}
 const config = {};
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
